b571538cf38f4c18c3ef079324ac91b6e448c6fb
Tom Catshoek <tomcatshoek@zeelandnet.nl>
2023-12-10 15:46:44 +0100

    Day 10 part 1 not great

--- day10/src/bin/part1.rs
+++ day10/src/bin/part1.rs
@@ -1,6 +1,7 @@
 #![feature(let_chains)]
 
 use std::collections::{BTreeMap, BTreeSet, VecDeque};
+use crate::Tile::SW;
 
 #[derive(Copy, Clone, Eq, PartialEq)]
 enum Tile {
@@ -32,14 +33,14 @@ impl Tile {
     fn next_positions(&self, cur_x: usize, cur_y: usize) -> Vec<(usize, usize)> {
         let x = cur_x as i32;
         let y = cur_y as i32;
-        
+
         let possible_positions = match self {
             Tile::NS => vec![(x, y - 1), (x, y + 1)],
             Tile::EW => vec![(x - 1, y), (x + 1, y)],
             Tile::NE => vec![(x, y - 1), (x + 1, y)],
             Tile::NW => vec![(x, y - 1), (x - 1, y)],
-            Tile::SW => vec![(x, y - 1), (x, y + 1)],
-            Tile::SE => vec![(x, y - 1), (x, y + 1)],
+            Tile::SW => vec![(x - 1, y), (x, y + 1)],
+            Tile::SE => vec![(x, y + 1), (x + 1, y)],
             Tile::GROUND => vec![],
             Tile::START => vec![(x, y - 1), (x, y + 1), (x + 1, y), (x - 1, y)]
         };
@@ -81,7 +82,7 @@ impl World {
             tiles,
             width,
             height,
-            start_pos
+            start_pos,
         }
     }
 
@@ -99,30 +100,28 @@ impl World {
     }
 }
 
-fn find_largest_distance(world: &World) -> ((usize, usize), i32) {
+fn walk_loop(world: &World, start_pos: (usize, usize)) -> BTreeMap<(usize, usize), usize> {
     let mut visited = BTreeMap::new();
     let mut to_visit = VecDeque::new();
 
-    let mut best_tile = (world.start_pos, 0);
-
-    to_visit.push_back((world.start_pos, 0));
+    visited.insert(world.start_pos, 0);
+    to_visit.push_back((start_pos, 1));
 
     while let Some(((x, y), cur_distance)) = to_visit.pop_front() {
-
-
+        // println!("Cur pos: {}, {}", x, y);
         visited.insert((x, y), cur_distance);
-
-        if cur_distance > best_tile.1 {
-            best_tile = ((x, y), cur_distance);
-        }
 
         let cur_tile = world.get(x, y).unwrap();
 
+        if cur_tile == Tile::GROUND {
+            panic!();
+        }
+
         for (new_x, new_y) in cur_tile.next_positions(x, y) {
 
-            // Only revisit if the current path is longer
-            if let Some(distance) = visited.get(&(new_x, new_y)) && *distance > cur_distance + 1{
-                continue
+            // Don't revisit on this walk
+            if visited.contains_key(&(new_x, new_y)) {
+                continue;
             }
 
             // Only visit within bounds of map & valid tile types
@@ -133,15 +132,75 @@ fn find_largest_distance(world: &World) -> ((usize, usize), i32) {
 
             to_visit.push_back(((new_x, new_y), cur_distance + 1))
         }
-
-        println!("To visit: {:?}", to_visit);
     }
 
-    best_tile
+    let max_dist = visited.values().max().unwrap();
+    println!("Loop len: {}", max_dist);
+
+    visited
+}
+
+fn find_largest_distance(world: &World) -> ((usize, usize), usize) {
+    let (start_x, start_y) = world.start_pos;
+
+    let mut loop_start_positions: Vec<(usize, usize)> = Vec::new(); //world.get(start_x, start_y)
+
+    if let Some(tile) = world.get(start_x + 1, start_y) {
+        if [Tile::EW, Tile::NW, Tile::SW].contains(&tile) {
+            loop_start_positions.push((start_x + 1, start_y));
+        }
+    }
+
+    if start_x > 0 {
+        if let Some(tile) = world.get(start_x - 1, start_y) {
+            if [Tile::EW, Tile::NE, Tile::SE].contains(&tile) {
+                loop_start_positions.push((start_x - 1, start_y));
+            }
+        }
+    }
+
+    if let Some(tile) = world.get(start_x, start_y + 1) {
+        if [Tile::NS, Tile::NW, Tile::NE].contains(&tile) {
+            loop_start_positions.push((start_x, start_y + 1));
+        }
+    }
+
+    if start_y > 0 {
+        if let Some(tile) = world.get(start_x, start_y - 1) {
+            if [Tile::NS, Tile::SW, Tile::SE].contains(&tile) {
+                loop_start_positions.push((start_x, start_y - 1));
+            }
+        }
+    }
+
+    let walk_distances: Vec<BTreeMap<(usize, usize), usize>> = loop_start_positions.iter()
+        .map(|pos| walk_loop(world, *pos))
+        .collect();
+
+    let mut max_distances = BTreeMap::new();
+
+    for distances in walk_distances {
+        for ((x, y), d) in distances {
+            max_distances.entry((x, y))
+                .and_modify(|cur_best| { if d < *cur_best { *cur_best = d } })
+                .or_insert(d);
+        }
+    }
+
+    let mut best_dist = ((start_x, start_y), 0);
+    for ((x, y), d) in max_distances {
+        if d > best_dist.1 {
+            best_dist = ((x, y), d);
+        }
+    }
+    best_dist
 }
 
 fn main() {
-    println!("Hello, world!");
+    let input = include_str!("../input1.txt");
+    let world = World::new(input);
+    let (_, dist) = find_largest_distance(&world);
+    println!("Result: {}", dist);
 }
 
 #[cfg(test)]
@@ -158,8 +217,8 @@ mod tests {
 
         let world = World::new(input);
 
-        let (_, dist) = find_largest_distance(&world);
-
+        let ((x, y), dist) = find_largest_distance(&world);
+        println!("{:?}", ((x, y), dist));
         assert_eq!(dist, 4);
     }
 
@@ -174,7 +233,7 @@ LJ...";
         let world = World::new(input);
 
         let ((x, y), dist) = find_largest_distance(&world);
-
+        println!("{:?}", ((x, y), dist));
         assert_eq!(dist, 8);
     }
 }
